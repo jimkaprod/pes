6294b40365c3064c4255e8dd30c10692893b313d
jmcordier <jmcordier@laprovence.com>
2018-07-27 16:57:31 +0200

    creation des tables et classes teams, competitions ...

--- app/teams/models.py
+++ app/teams/models.py
@@ -3,16 +3,14 @@ from .. import db
 
 class Teams(db.Model):
   __tablename__ = 'game_teams'
-  team_id = db.Column(db.Integer, primary_key=True)
-  team_type_id = db.Column(db.Integer, db.ForeignKey('teams_type.id'))
-  zone_id = db.Column(db.Integer, db.ForeignKey('zones.id'))
-  country_id = db.Column(db.Integer, db.ForeignKey('country.id'))
-  real_name = db.Column(db.String(64), unique=True)
-  game_name = db.Column(db.String(64), unique=True)
-  abbreviation = db.Column(db.String(64), unique=True)
-  # default = db.Column(db.Boolean, default=False, index=True)
-  # permissions = db.Column(db.Integer)
-  # users = db.relationship('User', backref='role', lazy='dynamic')
+  __table_args__ = {'extend_existing': True} 
+  id = db.Column(db.Integer, primary_key=True)
+  game_name = db.Column(db.String(64))
+  real_name = db.Column(db.String(64))
+  abbreviation = db.Column(db.String(64))
+  teams_types_id = db.Column(db.Integer, db.ForeignKey('teams_types.id'))
+  countries_id = db.Column(db.Integer, db.ForeignKey('countries.id'))
+  zones_id = db.Column(db.Integer, db.ForeignKey('zones.id'))
 
   # def __init__(self, **kwargs):
   #   super(Role, self).__init__(**kwargs)
@@ -22,6 +20,7 @@ class Teams(db.Model):
 
 class Zones(db.Model):
   __tablename__ = 'zones'
+  __table_args__ = {'extend_existing': True} 
   id = db.Column(db.Integer, primary_key=True)
   name = db.Column(db.String(64), unique=True)
   abbreviation = db.Column(db.String(3), unique=True)
@@ -32,8 +31,9 @@ class Zones(db.Model):
   #     self.permissions = 0
 
 
-class TeamsType(db.Model):
-  __tablename__ = 'teams_type'
+class TeamsTypes(db.Model):
+  __tablename__ = 'teams_types'
+  __table_args__ = {'extend_existing': True} 
   id = db.Column(db.Integer, primary_key=True)
   type = db.Column(db.String(64), unique=True)
 
@@ -42,16 +42,83 @@ class TeamsType(db.Model):
   #   if self.permissions is None:
   #     self.permissions = 0
 
-
-class Country(db.Model):
-  __tablename__ = 'teams_type'
+class Countries(db.Model):
+  __tablename__ = 'countries'
+  __table_args__ = {'extend_existing': True} 
   id = db.Column(db.Integer, primary_key=True)
   name = db.Column(db.String(64), unique=True)
   abbreviation = db.Column(db.String(3), unique=True)
+  zones_id = db.Column(db.Integer, db.ForeignKey('zones.id'))
 
   # def __init__(self, **kwargs):
   #   super(Role, self).__init__(**kwargs)
   #   if self.permissions is None:
   #     self.permissions = 0
+
+class Competitions(db.Model):
+  __tablename__ = 'competitions'
+  __table_args__ = {'extend_existing': True} 
+  id = db.Column(db.Integer, primary_key=True)
+  game_name = db.Column(db.String(64))
+  real_name = db.Column(db.String(64))
+  teams_types_id = db.Column(db.Integer, db.ForeignKey('teams_types.id'))
+  competitions_types_id = db.Column(db.Integer, db.ForeignKey('competitions_types.id'))
+  competitions_categories_id = db.Column(db.Integer, db.ForeignKey('competitions_categories.id'))
+  countries_id = db.Column(db.Integer, db.ForeignKey('countries.id'))
+  zones_id = db.Column(db.Integer, db.ForeignKey('zones.id'))
+
+  # def __init__(self, **kwargs):
+  #   super(Role, self).__init__(**kwargs)
+  #   if self.permissions is None:
+  #     self.permissions = 0
+
+class CompetitionsSeasons(db.Model):
+  __tablename__ = 'competitions_seasons'
+  __table_args__ = {'extend_existing': True} 
+  id = db.Column(db.Integer, primary_key=True)
+  seasons_id = db.Column(db.Integer, db.ForeignKey('seasons.id'))
+  competitions_id = db.Column(db.Integer, db.ForeignKey('competitions.id'))
+  teams_id = db.Column(db.Integer, db.ForeignKey('game_teams.id'))
+
+  # def __init__(self, **kwargs):
+  #   super(Role, self).__init__(**kwargs)
+  #   if self.permissions is None:
+  #     self.permissions = 0
+
+
+class CompetitionsTypes(db.Model):
+  __tablename__ = 'competitions_types'
+  __table_args__ = {'extend_existing': True} 
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String(64))
+
+  # def __init__(self, **kwargs):
+  #   super(Role, self).__init__(**kwargs)
+  #   if self.permissions is None:
+  #     self.permissions = 0
+
+class CompetitionsCategories(db.Model):
+  __tablename__ = 'competitions_categories'
+  __table_args__ = {'extend_existing': True} 
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String(64))
+
+  # def __init__(self, **kwargs):
+  #   super(Role, self).__init__(**kwargs)
+  #   if self.permissions is None:
+  #     self.permissions = 0
+
+
+class Seasons(db.Model):
+  __tablename__ = 'seasons'
+  __table_args__ = {'extend_existing': True} 
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String(64))
+
+  # def __init__(self, **kwargs):
+  #   super(Role, self).__init__(**kwargs)
+  #   if self.permissions is None:
+  #     self.permissions = 0
+
 
 
--- app/teams/views.py
+++ app/teams/views.py
@@ -1,16 +1,56 @@
 from flask import render_template, session, redirect, url_for, current_app
 from .. import db
-from .models import Teams
+from .models import Teams, Zones, TeamsTypes, Countries, Competitions, CompetitionsSeasons, CompetitionsTypes, CompetitionsCategories, Seasons
 from . import teams
 
 @teams.route('/home', methods=['GET', 'POST'])
 def index():
+    zones = Zones.query.order_by(Zones.id).all()
+    for zone in zones:
+        print(zone.id, zone.name)
+
+    teamsTypes = TeamsTypes.query.order_by(TeamsTypes.id).all()
+    for teamType in teamsTypes:
+        print(teamType.id, teamType.type)
+
+    countries = Countries.query.order_by(Countries.id).all()
+    for country in countries:
+        print(country.id, country.name, country.abbreviation, country.zones_id)
+
+    teams = Teams.query.order_by(Teams.id).all()
+    for team in teams:
+        print(team.id, team.game_name, team.real_name, team.abbreviation, team.teams_types_id, team.countries_id, team.zones_id)
+
+    competitions = Competitions.query.order_by(Competitions.id).all()
+    for competition in competitions:
+        print(competition.id, competition.game_name, competition.real_name, competition.teams_types_id, competition.competitions_types_id, competition.competitions_categories_id, competition.countries_id, competition.zones_id)
+
+    competitionsSeasons = CompetitionsSeasons.query.order_by(CompetitionsSeasons.id).all()
+    for competition in competitionsSeasons:
+        print(competition.id, competition.seasons_id, competition.competitions_id)
+
+    competitionsCategories = CompetitionsCategories.query.order_by(CompetitionsCategories.id).all()
+    for competition in competitionsCategories:
+        print(competition.id, competition.name)
+
+    seasons = Seasons.query.order_by(Seasons.id).all()
+    for season in seasons:
+        print(season.id, season.name)
+
+
+
+
+
+
     return render_template('teams/index.html')
 
 @teams.route('/team/<int:id>', methods=['GET', 'POST'])
 def team(id):
-    print('toto')
+
+
     post = Teams.query.get_or_404(id)
+
+
     return render_template('index.html', posts=[post])
 
     # form = TeamForm()

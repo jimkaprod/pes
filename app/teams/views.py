06c7a7b75870784c14d7177b67b961b8879709b1
jmcordier <jmcordier@laprovence.com>
2018-07-30 14:42:00 +0200

    requetes simples de toutes les tables

--- app/teams/views.py
+++ app/teams/views.py
@@ -5,44 +5,51 @@ from . import teams
 
 @teams.route('/home', methods=['GET', 'POST'])
 def index():
+
     zones = Zones.query.order_by(Zones.id).all()
-    for zone in zones:
-        print(zone.id, zone.name)
+    # for zone in zones:
+    #     print(zone.id, zone.name)
 
     teamsTypes = TeamsTypes.query.order_by(TeamsTypes.id).all()
-    for teamType in teamsTypes:
-        print(teamType.id, teamType.type)
+    # for teamType in teamsTypes:
+    #     print(teamType.id, teamType.type)
 
     countries = Countries.query.order_by(Countries.id).all()
-    for country in countries:
-        print(country.id, country.name, country.abbreviation, country.zones_id)
+    # for country in countries:
+    #     print(country.id, country.name, country.abbreviation, country.zones_id)
 
     teams = Teams.query.order_by(Teams.id).all()
     for team in teams:
         print(team.id, team.game_name, team.real_name, team.abbreviation, team.teams_types_id, team.countries_id, team.zones_id)
 
     competitions = Competitions.query.order_by(Competitions.id).all()
-    for competition in competitions:
-        print(competition.id, competition.game_name, competition.real_name, competition.teams_types_id, competition.competitions_types_id, competition.competitions_categories_id, competition.countries_id, competition.zones_id)
+    # for competition in competitions:
+    #     print(competition.id, competition.game_name, competition.real_name, competition.teams_types_id, competition.competitions_types_id, competition.competitions_categories_id, competition.countries_id, competition.zones_id)
 
     competitionsSeasons = CompetitionsSeasons.query.order_by(CompetitionsSeasons.id).all()
-    for competition in competitionsSeasons:
-        print(competition.id, competition.seasons_id, competition.competitions_id)
+    # for competition in competitionsSeasons:
+    #     print(competition.id, competition.seasons_id, competition.competitions_id)
 
     competitionsCategories = CompetitionsCategories.query.order_by(CompetitionsCategories.id).all()
-    for competition in competitionsCategories:
-        print(competition.id, competition.name)
+    # for competition in competitionsCategories:
+    #     print(competition.id, competition.name)
 
     seasons = Seasons.query.order_by(Seasons.id).all()
-    for season in seasons:
-        print(season.id, season.name)
+    # for season in seasons:
+    #     print(season.id, season.name)
 
 
 
-
-
-
-    return render_template('teams/index.html')
+    return render_template('teams/index.html',
+        zones=zones,
+        teamsTypes = teamsTypes,
+        countries = countries,
+        teams = teams,
+        competitions = competitions,
+        competitionsSeasons = competitionsSeasons,
+        competitionsCategories = competitionsCategories,
+        seasons = seasons
+    )
 
 @teams.route('/team/<int:id>', methods=['GET', 'POST'])
 def team(id):

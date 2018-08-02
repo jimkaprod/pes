be0effce0827a94cf55f4f5c22f26cb102a1e4b9
jmcordier <jmcordier@laprovence.com>
2018-08-02 17:17:19 +0200

    ajout data

--- app/teams/static/js/teams.js
+++ app/teams/static/js/teams.js
@@ -1,4 +1,4 @@
-/*
+
 function getXdr()
 {
     if (window.XDomainRequest && document.documentMode < 10) {
@@ -18,7 +18,7 @@ function htmlToElement(html) {
 function getTpl(tpl_name)
 {
   var request = getXdr();
-  request.open('GET', '/data/template/' + tpl_name);
+  request.open('GET', '/data/template/');
   request.responseType = "text";
   request.send();
 
@@ -39,12 +39,15 @@ function getTpl(tpl_name)
 }
 
 
-function dataRequest(tpl)
+function dataRequest()
 {
   var request = getXdr();
-  request.open('GET', '/data/resume');
+  // var queryString = Object.keys(json).map(key => key + '=' + json[key]).join('&');
+  request.open('POST', '/teams/teams/filters');
+  jsonData = JSON.stringify(dataFilters);
+  console.log(jsonData )
   request.responseType = "json";
-  request.send();
+  request.send(jsonData);
 
   request.onerror = function(e) {
   };
@@ -53,25 +56,24 @@ function dataRequest(tpl)
     if (request.status >= 200 && request.status < 400) {
       // Success!
       var response = request.response;
-
       data = response.data
-      container = document.getElementById('results')
-      container.innerHTML = tpl
-      source   = document.getElementById("entry-template").innerHTML;
+      // container = document.getElementById('results')
+      // container.innerHTML = tpl
+      // source   = document.getElementById("entry-template").innerHTML;
 
-      pagesVues = data['pagesVues'];
-      visites = data['visites'];
-      visiteurs = data['visiteurs'];
+      // pagesVues = data['pagesVues'];
+      // visites = data['visites'];
+      // visiteurs = data['visiteurs'];
 
-      var template = Handlebars.compile(source);
-      var context = {data};
-      var html = template(context);
+      // var template = Handlebars.compile(source);
+      // var context = {data};
+      // var html = template(context);
 
-      container.innerHTML=html;
-      container.getElementsByTagName('table')[0].id="id0"
+      // container.innerHTML=html;
+      // container.getElementsByTagName('table')[0].id="id0"
 
-      for(var i=0; i<data.length; i++){
-      }
+      // for(var i=0; i<data.length; i++){
+      // }
 
     } else {
       // We reached our target server, but it returned an error
@@ -85,40 +87,100 @@ Handlebars.registerHelper("inc", function(value, options)
   return parseInt(value) + 1;
 });
 
-document.addEventListener("DOMContentLoaded", function(e) {
-  btn = document.getElementById('btn-direction')
-  getTpl('_table_simple.html')
-});
-
 
 function translate() {
     console.log('zboub')
 }
-*/
 
 teamsFilters={}
-teamsFilters["js-teams-zones"] = -1;
-teamsFilters["js-teams-teams-types"] = -1;
-teamsFilters["js-teams-seasons"] = -1;
-teamsFilters["js-teams-countries"] = -1;
-teamsFilters["js-teams-teams"] = -1;
-teamsFilters["js-teams-competitions"] = -1;
-teamsFilters["js-teams-competitions-categories"] = -1;
-teamsFilters["js-teams-competitions-seasons"] = -1;
+teamsFilters["js-teams-competitions"] = {};
+teamsFilters["js-teams-competitions"]['id'] = -1;
+teamsFilters["js-teams-competitions"]['model'] = "Competitions";
+teamsFilters["js-teams-competitions-types"] = {};
+teamsFilters["js-teams-competitions-types"]['id'] = -1;
+teamsFilters["js-teams-competitions-types"]['model'] = "CompetitionsTypes";
+teamsFilters["js-teams-competitions-categories"] = {};
+teamsFilters["js-teams-competitions-categories"]['id'] = -1;
+teamsFilters["js-teams-competitions-categories"]['model'] = "CompetitionsCategories";
+teamsFilters["js-teams-seasons"] = {};
+teamsFilters["js-teams-seasons"]['id'] = -1;
+teamsFilters["js-teams-seasons"]['model'] = "Seasons";
+teamsFilters["js-teams-zones"] = {};
+teamsFilters["js-teams-zones"]['id'] = -1;
+teamsFilters["js-teams-zones"]['model'] = "Zones";
+teamsFilters["js-teams-countries"] = {};
+teamsFilters["js-teams-countries"]['id'] = -1;
+teamsFilters["js-teams-countries"]['model'] = "Countries";
+teamsFilters["js-teams-teams-types"] = {};
+teamsFilters["js-teams-teams-types"]['id'] = -1;
+teamsFilters["js-teams-teams-types"]['model'] = "TeamsTypes";
+teamsFilters["js-teams-teams"] = {};
+teamsFilters["js-teams-teams"]['id'] = -1;
+teamsFilters["js-teams-teams"]['model'] = "Teams";
+
+
+dataFilters={}
+// teamsFilters["js-teams-zones"] = -1;
+// teamsFilters["js-teams-teams-types"] = -1;
+// teamsFilters["js-teams-seasons"] = -1;
+// teamsFilters["js-teams-countries"] = -1;
+// teamsFilters["js-teams-teams"] = -1;
+// teamsFilters["js-teams-competitions-categories"] = -1;
+// teamsFilters["js-teams-competitions-seasons"] = -1;
+
+// document.addEventListener("DOMContentLoaded", function(e) {
+//   btn = document.getElementById('btn-direction')
+//   getTpl('_table_simple.html')
+// });
+
+var event = new Event('build');
+
+//Ecouter l'événement.
+document.addEventListener('build', function (e) { console.log('allo jecoute') }, false);
+
+//distribuer l'événement.
+document.dispatchEvent(event);
+
+
+var eCompetitionsFiltersChange = new Event('onCompetitionsFiltersChange');
 
 document.addEventListener("DOMContentLoaded", function(e) {
   var loop = Object.keys(teamsFilters).length;
+
+  console.log('------------------------------')
+  console.log(teamsFilters)
+  console.log('------------------------------')
   for (var k in teamsFilters) {
     select = document.getElementById(k);
-    select.onchange=changeEventHandler;
+    console.log('----->>', k, teamsFilters[k]['model'], teamsFilters[k]['id'])
+    dataFilters[ teamsFilters[k]['model']] =  teamsFilters[k]['id']
+    // select.onchange=changeEventHandler;
+    console.log(dataFilters)
+    select.addEventListener('change', function(e) {
+      res = changeEventHandler(e);
+      // eCompetitionsFiltersChange.data = teamsFilters
+      document.dispatchEvent(eCompetitionsFiltersChange);
+    }, false);
   }
 });
 
-function changeEventHandler(event) {
-  target = this[this.selectedIndex].dataset;
-  for (var k in target){
-    if (typeof target[k] !== 'function') {
-      console.log("Key is " + k + ", value is " + target[k]);
+document.addEventListener("onCompetitionsFiltersChange", function(e) {
+  dataRequest()
+});
+
+
+function changeEventHandler(e) {
+  var target = e.currentTarget;
+  var dataset = e.currentTarget[e.currentTarget.selectedIndex].dataset;
+  var model = target.dataset['model']
+  var res = {}
+  res['model'] = model
+  for (var k in dataset){
+    if (typeof dataset[k] !== 'function' && k == 'id') {
+      dataFilters[model] =  parseInt(dataset[k])
+      break
     }
   }
 }
+
+
--- app/teams/views.py
+++ app/teams/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, session, redirect, url_for, current_app
+from flask import render_template, session, redirect, url_for, current_app, jsonify, json, request
 from .. import db
 from .models import Teams, Zones, TeamsTypes, Countries, Competitions, CompetitionsTypes, CompetitionsCategories, Seasons
 from . import teams
@@ -6,58 +6,72 @@ from . import teams
 @teams.route('/home', methods=['GET', 'POST'])
 def index():
 
-    zones = Zones.query.order_by(Zones.id).all()
+    competitions = {}
+    competitionsList = Competitions.query.order_by(Competitions.id).all()
+    competitions['list'] = competitionsList
+    competitions['model'] = 'Competitions'
+    # for competition in competitionsList:
+    #     print(competition.id, competition.game_name, competition.real_name, competition.teams_types_id, competition.competitions_types_id, competition.competitions_categories_id, competition.countries_id, competition.zones_id)
+
+
+    competitionsTypes = {}
+    competitionsTypesList = CompetitionsTypes.query.order_by(CompetitionsTypes.id).all()
+    competitionsTypes['list'] = competitionsTypesList
+    competitionsTypes['model'] = 'CompetitionsTypes'
+    # for competitionTypes in competitionsTypes:
+    #     print(competitionTypes.id, competitionTypes.name)
+
+    competitionsCategories = {}
+    competitionsCategoriesList = CompetitionsCategories.query.order_by(CompetitionsCategories.id).all()
+    competitionsCategories['list'] = competitionsCategoriesList
+    competitionsCategories['model'] = 'CompetitionsCategories'
+    # for competition in competitionsCategories:
+    #     print(competition.id, competition.name)
+
+
+    seasons = {}
+    seasonsList = Seasons.query.order_by(Seasons.id).all()
+    seasons['list'] = seasonsList
+    seasons['model'] = 'Seasons'
+    # for season in seasons:
+    #     print(season.id, season.name)
+
+    zones = {}
+    zonesList = Zones.query.order_by(Zones.id).all()
+    zones['list'] = zonesList
+    zones['model'] = 'Zones'
     # for zone in zones:
     #     print(zone.id, zone.name)
 
-    teamsTypes = TeamsTypes.query.order_by(TeamsTypes.id).all()
-    # for teamType in teamsTypes:
-    #     print(teamType.id, teamType.type)
+    countries = {}
+    countriesList = Countries.query.order_by(Countries.id).all()
+    countries['list'] = countriesList
+    countries['model'] = 'Countries'
 
-    countries = Countries.query.order_by(Countries.id).all()
     # for country in countries:
     #     print(country.id, country.name, country.abbreviation, country.zones_id)
 
-    teams = Teams.query.order_by(Teams.id).all()
+    teamsTypes = {}
+    teamsTypesList = TeamsTypes.query.order_by(TeamsTypes.id).all()
+    teamsTypes['list'] = teamsTypesList
+    teamsTypes['model'] = 'TeamsTypes'
+    # for teamType in teamsTypes:
+    #     print(teamType.id, teamType.type)
+
+    teams = {}
+    teamsList = Teams.query.order_by(Teams.id).all()
+    teams['list'] = teamsList
+    teams['model'] = 'Teams'
     # for team in teams:
     #     print(team.id, team.game_name, team.real_name, team.abbreviation, team.teams_types_id, team.countries_id, team.zones_id)
-
-    competitions = Competitions.query.order_by(Competitions.id).all()
-    # for competition in competitions:
-    #     print(competition.id, competition.game_name, competition.real_name, competition.teams_types_id, competition.competitions_types_id, competition.competitions_categories_id, competition.countries_id, competition.zones_id)
 
     # competitionsSeasons = CompetitionsSeasons.query.order_by(CompetitionsSeasons.id).all()
     # for competition in competitionsSeasons:
     #     print(competition.id, competition.seasons_id, competition.competitions_id)
 
-    competitionsCategories = CompetitionsCategories.query.order_by(CompetitionsCategories.id).all()
-    # for competition in competitionsCategories:
-    #     print(competition.id, competition.name)
-
-    seasons = Seasons.query.order_by(Seasons.id).all()
-    for season in seasons:
-        print(season.id, season.name)
-
     # userList = users.query.join(friendships, users.id==friendships.user_id).add_columns(users.userId, users.name, users.email, friends.userId, friendId).filter(users.id == friendships.friend_id).filter(friendships.user_id == userID).paginate(page, 1, False)
     # userList = CompetitionsTypes.query.join(friendships, users.id==friendships.user_id).add_columns(users.userId, users.name, users.email, friends.userId, friendId).filter(users.id == friendships.friend_id).filter(friendships.user_id == userID).paginate(page, 1, False)
 
-    q1 = CompetitionsTypes.query.join(Competitions, CompetitionsTypes.id==Competitions.competitions_types_id) \
-    .filter(CompetitionsTypes.name == 'championship') \
-    .all()
-
-    q2 = Competitions.query.join(CompetitionsTypes, CompetitionsTypes.id==Competitions.competitions_types_id) \
-    .filter(CompetitionsTypes.name == 'championship') \
-    .all()
-
-    print('### Q1:')
-    for item in q1:
-        print(item.name)
-    print('')
-
-    print('### Q2:')
-    for item in q2:
-        print(item.game_name,item.real_name)
-    print('')
 
     return render_template('teams/index.html',
         zones=zones,
@@ -67,8 +81,68 @@ def index():
         competitions = competitions,
         # competitionsSeasons = competitionsSeasons,
         competitionsCategories = competitionsCategories,
-        seasons = seasons
+        seasons = seasons,
+        competitionsTypes = competitionsTypes
     )
+
+
+@teams.route('/teams/filters', methods=['POST'])
+def teamsFilters():
+    data = json.loads(request.data)
+    print(data)
+    for k,v in data.items():
+        if(k == 'Competitions'):
+            CompetitionsId = v
+        elif(k == 'CompetitionsTypes'):
+            CompetitionsTypesId = v
+        elif(k == 'Seasons'):
+            SeasonsId = v
+        print(k,v)
+
+    if(CompetitionsTypesId == -1):
+        competitionsTypesFilter = 'CompetitionsTypes.id != CompetitionsTypesId'
+    else:
+        competitionsTypesFilter = 'CompetitionsTypes.id == CompetitionsTypesId'
+
+    q = Competitions.query \
+    .join(CompetitionsTypes, CompetitionsTypes.id==Competitions.competitions_types_id) \
+    .add_columns(Competitions.game_name, Competitions.real_name) \
+    .filter(eval(competitionsTypesFilter)) \
+    .all()
+
+    print('### Q:')
+    for item in q:
+        print(item.game_name, item.real_name)
+    print('')
+
+
+    # q1 = CompetitionsTypes.query.join(Competitions, CompetitionsTypes.id==Competitions.competitions_types_id) \
+    # .filter(CompetitionsTypes.name == 'championship') \
+    # .all()
+
+
+    # print('### Q1:')
+    # for item in q1:
+    #     print(item.name)
+    # print('')
+
+    # print('### Q2:')
+    # for item in q2:
+    #     print(item.game_name,item.real_name)
+    # print('')
+
+
+
+    return jsonify(data)
+    # i = data.get('id')
+    # m = data.get('model')
+
+    # val = jsonify(
+    #     id=i,
+    #     model=m
+    # )
+
+    # return val
 
 @teams.route('/team/<int:id>', methods=['GET', 'POST'])
 def team(id):

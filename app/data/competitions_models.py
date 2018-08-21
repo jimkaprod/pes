363eb5f18c7db874d94980aa544cf8065f9cf1eb
cordier jean michel <jimkaprod@MacBook-Pro-de-cordier.local>
2018-08-21 20:08:14 +0200

    modification du model

--- app/data/competitions_models.py
+++ app/data/competitions_models.py
@@ -1,36 +1,20 @@
 from flask import current_app
 from .. import db
 
-
-
-# competitions = db.Table('competitions',
-#     db.Column('id', db.Integer, primary_key=True, autoincrement=True),
-#     db.Column('videos_games_names_id', db.Integer, db.ForeignKey('videos_games_names.id')),
-#     db.Column('seasons_names_id', db.Integer, db.ForeignKey('seasons_names.id')),
-#     db.Column('zones_names_id', db.Integer, db.ForeignKey('zones_names.id')),
-#     db.Column('countries_names_id', db.Integer, db.ForeignKey('countries_names.id')),
-#     db.Column('competitions_types_id', db.Integer, db.ForeignKey('competitions_types.id')),
-#     db.Column('competitions_categories_id', db.Integer, db.ForeignKey('competitions_categories.id')),
-#     db.Column('competitions_names_id', db.Integer, db.ForeignKey('competitions_names.id')),
-#     db.Column('teams_names_id', db.Integer, db.ForeignKey('teams_names.id')),
-#     db.Column('teams_types_id', db.Integer, db.ForeignKey('teams_types.id')),
-#     db.Column('teams_levels_id', db.Integer, db.ForeignKey('teams_levels.id')),
-# )
-
 class Competitions(db.Model):
   __tablename__ = 'competitions'
   __table_args__ = {'extend_existing': True}
   id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-  videos_games_names_id = db.Column(db.Integer, db.ForeignKey('videos_games_names.id'))
-  seasons_names_id = db.Column(db.Integer, db.ForeignKey('seasons_names.id'))
-  zones_names_id = db.Column(db.Integer, db.ForeignKey('zones_names.id'))
-  countries_names_id = db.Column(db.Integer, db.ForeignKey('countries_names.id'))
-  competitions_types_id = db.Column(db.Integer, db.ForeignKey('competitions_types.id'))
-  competitions_categories_id = db.Column(db.Integer, db.ForeignKey('competitions_categories.id'))
-  competitions_names_id = db.Column(db.Integer, db.ForeignKey('competitions_names.id'))
-  teams_names_id = db.Column(db.Integer, db.ForeignKey('teams_names.id'))
-  teams_types_id = db.Column(db.Integer, db.ForeignKey('teams_types.id'))
-  teams_levels_id = db.Column(db.Integer, db.ForeignKey('teams_levels.id'))
+  videos_games_names_id = db.Column(db.Integer)
+  seasons_names_id = db.Column(db.Integer)
+  zones_names_id = db.Column(db.Integer)
+  countries_names_id = db.Column(db.Integer)
+  competitions_types_id = db.Column(db.Integer)
+  competitions_categories_id = db.Column(db.Integer)
+  competitions_names_id = db.Column(db.Integer)
+  teams_names_id = db.Column(db.Integer)
+  teams_types_id = db.Column(db.Integer)
+  teams_levels_id = db.Column(db.Integer)
 
 
 class VideosGamesNames(db.Model):
@@ -38,12 +22,14 @@ class VideosGamesNames(db.Model):
   __table_args__ = {'extend_existing': True}
   id = db.Column(db.Integer, primary_key=True)
   name = db.Column(db.String(64))
+  competition_videos_games_names_id = db.Column(db.Integer, db.ForeignKey('competitions.videos_games_names_id'), nullable=False)
 
 class SeasonsNames(db.Model):
   __tablename__ = 'seasons_names'
   __table_args__ = {'extend_existing': True}
   id = db.Column(db.Integer, primary_key=True)
   name = db.Column(db.String(64))
+  competition_seasons_names_id = db.Column(db.Integer, db.ForeignKey('competitions.seasons_names_id'), nullable=False)
 
 class ZonesNames(db.Model):
   __tablename__ = 'zones_names'
@@ -51,9 +37,7 @@ class ZonesNames(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   name = db.Column(db.String(64), unique=True)
   abbreviation = db.Column(db.String(3), unique=True)
-  # team = db.relationship('Teams', backref='teamZones', lazy=True)
-  # competition = db.relationship('Competitions', backref='competitionZones', lazy=True)
-  # country = db.relationship('Countries', backref='countryZones', lazy=True)
+  competition_zones_names_id = db.Column(db.Integer, db.ForeignKey('competitions.zones_names_id'), nullable=False)
 
 class CountriesNames(db.Model):
   __tablename__ = 'countries_names'
@@ -61,31 +45,25 @@ class CountriesNames(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   name = db.Column(db.String(64), unique=True)
   abbreviation = db.Column(db.String(3), unique=True)
-  # zones_id = db.Column(db.Integer, db.ForeignKey('zones.id'))
-  # team = db.relationship('Teams', backref='teamCountries', lazy=True)
-  # competition = db.relationship('Competitions', backref='competitionCountries', lazy=True)
+  competition_countries_names_id = db.Column(db.Integer, db.ForeignKey('competitions.countries_names_id'), nullable=False)
 
 class CompetitionsTypes(db.Model):
   __tablename__ = 'competitions_types'
   __table_args__ = {'extend_existing': True}
   id = db.Column(db.Integer, primary_key=True)
   name = db.Column(db.String(64))
-  # competition = db.relationship('Competitions', backref='competitionCompetitionsTypes', lazy=True)
+  competitions_competitions_types_id = db.Column(db.Integer, db.ForeignKey('competitions.competitions_types_id'), nullable=False)
 
 class CompetitionsCategories(db.Model):
   __tablename__ = 'competitions_categories'
   __table_args__ = {'extend_existing': True}
   id = db.Column(db.Integer, primary_key=True)
   name = db.Column(db.String(64))
-  # competition = db.relationship('Competitions', backref='competitionCompetitionsCategories', lazy=True)
+  competitions_competitions_categories_id = db.Column(db.Integer, db.ForeignKey('competitions.competitions_categories_id'), nullable=False)
 
 class CompetitionsNames(db.Model):
   __tablename__ = 'competitions_names'
   __table_args__ = {'extend_existing': True}
   id = db.Column(db.Integer, primary_key=True)
   name = db.Column(db.String(64))
-  # teams_types_id = db.Column(db.Integer, db.ForeignKey('teams_types.id'))
-  # competitions_types_id = db.Column(db.Integer, db.ForeignKey('competitions_types.id'))
-  # competitions_categories_id = db.Column(db.Integer, db.ForeignKey('competitions_categories.id'))
-  # countries_id = db.Column(db.Integer, db.ForeignKey('countries.id'))
-  # zones_id = db.Column(db.Integer, db.ForeignKey('zones.id'))
+  competitions_competitions_names_id = db.Column(db.Integer, db.ForeignKey('competitions.competitions_names_id'), nullable=False)
--- app/data/teams_models.py
+++ app/data/teams_models.py
@@ -1,6 +1,6 @@
 from flask import current_app
 from .. import db
-
+from .competitions_models import Competitions
 
 class TeamsNames(db.Model):
   __tablename__ = 'teams_names'
@@ -9,16 +9,19 @@ class TeamsNames(db.Model):
   real_name = db.Column(db.String(64))
   fifa_name = db.Column(db.String(64))
   pes_name = db.Column(db.String(64))
+  competitions_teams_names_id = db.Column(db.Integer, db.ForeignKey('competitions.teams_names_id'), nullable=False)
 
 class TeamsTypes(db.Model):
   __tablename__ = 'teams_types'
   __table_args__ = {'extend_existing': True}
   id = db.Column(db.Integer, primary_key=True)
   type = db.Column(db.String(64))
+  competitions_teams_types_id = db.Column(db.Integer, db.ForeignKey('competitions.teams_types_id'), nullable=False)
 
 class TeamsLevels(db.Model):
   __tablename__ = 'teams_levels'
   __table_args__ = {'extend_existing': True}
   id = db.Column(db.Integer, primary_key=True)
   level = db.Column(db.Float, unique=True)
+  competitions_teams_levels_id = db.Column(db.Integer, db.ForeignKey('competitions.teams_levels_id'), nullable=False)
 
